65b1e6232149488902014b21b2ce01fd43340eae
vighnesh777 <vighneshkanakala777@gmail.com>
2021-04-19 16:47:58 +0530

    A misunderstanding guys----pushed previous file to git before and now im pushing the login.php file with php code included inside the code itself

---
+++ login.php
@@ -0,0 +1,85 @@
+<?php
+$username = filter_input(INPUT_POST, 'Email');
+$password = filter_input(INPUT_POST, 'Password');
+if (!empty($username)){
+if (!empty($password)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "1234";
+$dbname = "login_db";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO login_db (username, password)
+values ('$username','$password')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+    <form method="POST" action="#">
+        <div class="d-flex justify-content-center mt-5" >
+        <div >
+        <div >
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="Email">
+          <div id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mt-5">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" name="Password">
+        </div>
+        <div id="passwordHelpBlock" class="form-text justify-content ml-3 text-muted">
+             Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+          </div>
+          <div class="d-flex justify-content-center">
+              <button type="submit" class="btn btn-primary mt-3 ">Submit</button>
+          </div>
+          <div class="d-flex justify-content-center mt-3"><a href="html/forgotpassword.html">Forgot Password?</a></div>
+          
+        </div>
+    </div>
+      </form>
+  </body>
+</html>
