55644f7f935a9b89fafe8f8198953eb11a53357d
Karicharla GNV Swamy Naidu <ganeshkaricherla@gmail.com>
2021-04-21 19:36:17 +0530

    Added Navbar to Login page,Register Page,Forgotpage

--- html/registerpage.php
+++ html/registerpage.php
@@ -10,6 +10,12 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Register</a>
+    </div>
+  </nav>
   <?php
           if(@$_GET['is_empty']==true)
           {
