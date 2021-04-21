1287c8cd31e207df1fe4713337a9d2e180f4c08b
vighnesh777 <vighneshkanakala777@gmail.com>
2021-04-21 19:55:50 +0530

    done Index.php

--- index.php
+++ index.php
@@ -17,7 +17,37 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+    <nav class="navbar navbar-light bg-dark">
+        <div class="container ">
+           
+           <div class="offset-4 text-light fs-2"><h2>
+Inventory Management System</h2>
+      </div>
+        </div>
+      
+        <a href="html/loginpage.php" class="btn btn-primary btn-lg " tabindex="-1" role="button" >Login</a>
+    </nav>
+    <div class="pt-5 bg-secondary text-light"> 
+    <h4><p class="fw-bold offset-4">What is an inventory management system?</p></h4><br><br>
+An inventory management system (or inventory system) is the process by which you track your goods throughout your entire supply chain, from purchasing to production to end sales. It governs how you approach inventory management for your business.<br>
+<br>
+<h4><p class="fw-bold offset-5">Why do we need it?</p></h4><br>
+Any venture that handles stock will need a system to accurately track and control it. Without one, you’ll be working on an entirely ad-hoc basis — and you’ll quickly run into situations where your business is overstocked or understocked.<br>
+Inventory systems tell you the number of components or ingredients you need to create or assemble your final product. Without this information you may end up with excess stock, eroding your bottom line, or with insufficient stock to meet customer demand.<br>
+But while you will need an inventory management system, which one you choose is entirely up to you. There are countless different systems you can adopt, ranging from simple approaches to comprehensive solutions.
 
-    <a href="html/loginpage.php" class="btn btn-primary btn-lg " tabindex="-1" role="button" >Login</a>
+</div>
+<div>
+<div class="card mt-5 ml-3" style="width: 18rem;">
+  <img src="/assets/images/mypic2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">K.Vighnesh</h5>
+    <p class="card-text">Developer | Ethical Hacking Enthusiast | Designer | Keen Learner
+East Godavari, Andhra Pradesh, India</p>
+    <a href="https://www.linkedin.com/in/vighnesh-kanakala-4ab816190/" class="btn btn-primary">Linkedin</a>
+    <a href="https://github.com/vighnesh777" class="btn btn-primary">Github</a>
+  </div>
+</div>
+</div>
   </body>
 </html>
